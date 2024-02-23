8081aa04e4bc37a9b63e4200dd876769babb38a9
Kristiyan Nikolov <stu2101261038@uni-plovdiv.bg>
2024-02-23 11:45:09 +0200

    Initial commit

---
+++ src/main/java/pu/fmi/cources/FourInARowApplication.java
@@ -0,0 +1,13 @@
+package pu.fmi.cources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourInARowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourInARowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pu/fmi/cources/FourInARowApplicationTests.java
@@ -0,0 +1,13 @@
+package pu.fmi.cources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourInARowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
